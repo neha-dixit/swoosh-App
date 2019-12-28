852b41dbd2ec9f47a0e24a0f7e5832c495329ee8
Saurabh Dixit <saurabhdixit@saurabhs-macbook-pro.local>
2019-12-28 03:10:32 -0500

    welcome screen

---
+++ app.swoosh/BorderButton.swift
@@ -0,0 +1,26 @@
+//
+//  BorderButton.swift
+//  app.swoosh
+//
+//  Created by Saurabh Dixit on 12/28/19.
+//  Copyright © 2019 Dixit. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 3.0
+        layer.borderColor = UIColor.white.cgColor
+    }
+    
+}
